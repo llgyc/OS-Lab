150843b53fdda9b77f281471bd9697f688a343b7
llgyc <llgyc@pku.edu.cn>
2020-11-29 00:43:09 +0800

    Complete Exercise 6

--- kern/trap.c
+++ kern/trap.c
@@ -183,6 +183,8 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
+	if (tf->tf_trapno == T_BRKPT)
+		monitor(tf);
 	if (tf->tf_trapno == T_PGFLT)
 		page_fault_handler(tf);
 
