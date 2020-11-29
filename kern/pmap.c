f78287a2202290fa04e0e677057415807e61762b
llgyc <llgyc@pku.edu.cn>
2020-11-29 19:07:21 +0800

    Complete Exercise 9 & 10

--- kern/pmap.c
+++ kern/pmap.c
@@ -543,6 +543,23 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
+	if (env == NULL)
+		return -E_FAULT;
+	if (len == 0)
+		return 0;
+	char *start_va = ROUNDDOWN((char *)va, PGSIZE);
+	char *end_va = ROUNDUP((char *)va + len, PGSIZE);
+	if (start_va > end_va)
+		return -E_FAULT;
+	perm |= PTE_P;
+	for (; start_va < end_va; start_va += PGSIZE) {
+		pte_t *p_pte = pgdir_walk(env->env_pgdir, start_va, 0);
+		if ((uint32_t)start_va >= ULIM || !p_pte || ((*p_pte) & perm) != perm) {
+			user_mem_check_addr = (uint32_t)va < (uint32_t)start_va
+				? (uint32_t)start_va : (uint32_t)va;
+			return -E_FAULT;
+		}
+	}
 
 	return 0;
 }
